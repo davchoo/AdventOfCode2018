61ba4108f53e0eaafe6cf2db8b1581c3af5e422e
davchoo <davchoo3@gmail.com>
2018-12-11 18:49:45 -0800

    Cleaned up Day 10 Instead of adding the velocity to the position of each point every second and then placing those points in a dictionary and updating the point list, calculate the position using multiplication and immediately place it in a dictionary (Flattens 2 loops)
    Removed some debug prints
    Got rid of an off by one error

--- day_10_2018.py
+++ day_10_2018.py
@@ -1,63 +1,54 @@
 from aocd import get_data, submit
 
-import collections
-import itertools
 import re
-import sys
 
 
 def day10(submit_answer=False):
     data = get_data(day=10, year=2018).split("\n")
-    answer1 = 0
-    answer2 = 0
 
     regex = re.compile(r"(-?\d+)")
 
     points = []
 
-    # y neg up
-
     for point in data:
         pos_x, pos_y, vel_x, vel_y = map(int, regex.findall(point))
         points.append((pos_x, pos_y, vel_x, vel_y))
 
-    i = 0
-    last_under = -1
-    last_map = {}
+    current_time = 0
+    last_sky = {}
     last_width = 1000
 
     while True:
-        for j, (pos_x, pos_y, vel_x, vel_y) in enumerate(points):
-            points[j] = (pos_x + vel_x, pos_y + vel_y, vel_x, vel_y)
-        m = {}
-        for pos_x, pos_y, *rest in points:
-            m[(pos_x, pos_y)] = True
+        sky = {}
+        for pos_x, pos_y, vel_x, vel_y in points:
+            sky[(pos_x + vel_x * current_time, pos_y + vel_y * current_time)] = True
 
-        min_x = min(m.keys(), key=lambda x: x[0])[0]
-        max_x = max(m.keys(), key=lambda x: x[0])[0]
+        min_x = min(sky.keys(), key=lambda x: x[0])[0]
+        max_x = max(sky.keys(), key=lambda x: x[0])[0]
 
         width = max_x - min_x
 
+        # Print the previous sky if the width of the sky starts growing
         if last_width < width and last_width < 1000:
-            min_x = min(last_map.keys(), key=lambda x: x[0])[0]
-            min_y = min(last_map.keys(), key=lambda x: x[1])[1]
-            max_x = max(last_map.keys(), key=lambda x: x[0])[0]
-            max_y = max(last_map.keys(), key=lambda x: x[1])[1]
-            print(f"Time: {i} {width}")
+            min_x = min(last_sky.keys(), key=lambda x: x[0])[0]
+            min_y = min(last_sky.keys(), key=lambda x: x[1])[1]
+            max_x = max(last_sky.keys(), key=lambda x: x[0])[0]
+            max_y = max(last_sky.keys(), key=lambda x: x[1])[1]
 
             answer1 = "\n\t\t"
             for y in range(min_y, max_y + 1):
                 for x in range(min_x, max_x+1):
-                    if (x, y) in last_map:
+                    if (x, y) in last_sky:
                         answer1 += "#"
                     else:
                         answer1 += " "
                 answer1 += "\n\t\t"
+            current_time -= 1  # The time during the previous cycle
             break
         else:
             last_width = width
-            last_map = m
-        i += 1
+            last_sky = sky
+        current_time += 1
     if submit_answer:
-        submit(i, 2, day=10, year=2018)
-    return answer1, i
+        submit(current_time, 2, day=10, year=2018)
+    return answer1, current_time
