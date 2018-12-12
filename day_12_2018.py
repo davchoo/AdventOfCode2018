3f5cba7e1dbaa176c53ff1bfcdc0931b36f9537b
davchoo <davchoo3@gmail.com>
2018-12-11 23:14:19 -0800

    Cleaned up day 12
    Instead of looping over all the plants to sum their indexes, used sum
    and list comprehension
    Inlined vars that were used only once
    Removed first answer loop and used if to get the plant count after 20
    generations in the second loop

--- day_12_2018.py
+++ day_12_2018.py
@@ -4,46 +4,23 @@ from aocd import get_data, submit
 def day12(submit_answer=False):
     data = get_data(day=12, year=2018).split("\n")
 
-    init_state = {}
-    for i, state in enumerate(data[0].split()[2]):
-        init_state[i] = state
-
-    spread_data = data[2::]
+    init_state = {i: state for i, state in enumerate(data[0].split()[2])}
 
     spread = {}
-
-    for s in spread_data:
+    for s in data[2::]:
         state, __, target = s.split()
         spread[state] = target
 
     state = init_state.copy()
 
-    for j in range(1, 21):
-        new_state = {}
-        for i in range(-2+min(state.keys()), 2+max(state.keys())):
-            surroundings = "".join([state[i+o] if o+i in state else "." for o in range(-2, 3)])
-            if surroundings in spread:
-                new_state[i] = spread[surroundings]
-            else:
-                if i in state:
-                    new_state[i] = "."
-        state = new_state
-
-    answer1 = sum(i for i, pot in state.items() if pot == "#")
-
-    state = init_state.copy()
-
     diffs = []
-    last_plant_count = 0
+    last_plant_count = sum(i for i, pot in state.items() if pot == "#")
+    answer1 = 0
 
-    for k, v in state.items():
-        if v == "#":
-            last_plant_count += k
-
-    for j in range(1, 2000+1):
+    for j in range(0, 2000):
         new_state = {}
         for i in range(-2 + min(state.keys()), 2 + max(state.keys())):
-            surroundings = "".join([state[i + o] if o + i in state else "." for o in range(-2, 3)])
+            surroundings = "".join([state[i + o] if i + o in state else "." for o in range(-2, 3)])
             if surroundings in spread:
                 new_state[i] = spread[surroundings]
             else:
@@ -51,8 +28,11 @@ def day12(submit_answer=False):
                     new_state[i] = "."
         state = new_state
         plant_count = sum(i for i, pot in state.items() if pot == "#")
+        if j == 19:
+            answer1 = plant_count
         diffs.append(plant_count - last_plant_count)
         last_plant_count = plant_count
+        # If the current difference is equal to the previous 2 differences
         if len(diffs) > 3 and diffs[-1] == diffs[-2] and diffs[-1] == diffs[-3]:
             break
 
