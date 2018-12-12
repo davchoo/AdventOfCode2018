2eac144b9a4b5e40388cb00217492c89b192c368
davchoo <davchoo3@gmail.com>
2018-12-11 18:42:20 -0800

    Changed Day 11 to use 2D Partial Sums to significantly speed it up
    Also made sure there isn't whitespace in answer1
    
    Grid now stores sum of all cells from (1,1) to (x, y), the key
    
    The partial sum of an area from (x1, y1), the top left corner, to (x2, y2), the bottom right corner, is
      The sum of the area from (1, 1) to (x1 - 1, y1 - 1) or grid[(x1 - 1, y1 - 1)]
      minus the sum of the area from (1, 1) to (x1 - 1, y2 - 1) or grid[(x1 - 1, y2 -1)]
      minus the sum of the area from (1, 1) to (x2 - 1, y1 - 1) or grid[(x2 - 1, y1 - 1)]
      plus the sum of the area from (1, 1) to (x2 - 1, y2 - 1) or grid[(x2 - 1, y2 - 1)]
    
    This means finding the sum of any area takes 4 array/dictionary accesses
    
    A time reduction of 5 mins to ~9 seconds

--- day_11_2018.py
+++ day_11_2018.py
@@ -11,8 +11,8 @@ def day11(submit_answer=False):
 
     grid = collections.defaultdict(int)
 
-    for x in range(1, 301):
-        for y in range(1, 301):
+    for y in range(1, 301):
+        for x in range(1, 301):
             rack_id = x + 10
             power_level = rack_id * y + grid_serial
             power_level *= rack_id
@@ -20,42 +20,38 @@ def day11(submit_answer=False):
             power_level -= 5
             grid[(x, y)] = power_level
 
+            # Compute 2D Partial Sum Table
+            grid[(x, y)] += grid[(x - 1, y)]
+            grid[(x, y)] += grid[(x, y - 1)]
+            grid[(x, y)] -= grid[(x - 1, y - 1)]  # Remove double count
+
     max_fuel = 0
     max_fuel_loc = (0, 0)
 
-    for x in range(1, 301):
-        for y in range(1, 301):
-            if x+3 > 300 or y+3 > 300:
-                break
-            sum_fuel = 0
-            for ox in range(0, 3):
-                for oy in range(0, 3):
-                    sum_fuel += grid[(x + ox, y + oy)]
+    for x in range(0, 300-2):
+        for y in range(0, 300-2):
+            sum_fuel = grid[(x + 3, y + 3)] - grid[(x, y + 3)] - grid[(x + 3, y)] + grid[(x, y)]
             if sum_fuel > max_fuel:
                 max_fuel = sum_fuel
-                max_fuel_loc = (x, y)
+                max_fuel_loc = (x + 1, y + 1)
 
     max_fuel_2 = 0
     max_fuel_loc_2 = (0, 0)
     max_fuel_size_2 = 0
 
-    for x in range(1, 301):
-        for y in range(1, 301):
-            sum_fuel = grid[(x, y)]
-            for size in range(1, 300-max(x-1, y-1)):
-                for o in range(0, size-1):
-                    sum_fuel += grid[(x + o, y + size - 1)]
-                    sum_fuel += grid[(x + size - 1, y + o)]
-                if size > 1:
-                    sum_fuel += grid[(x + size - 1, y + size - 1)]
+    for x in range(0, 300):
+        for y in range(0, 300):
+            for size in range(1, 300-max(x, y)):
+                sum_fuel = grid[(x + size, y + size)] - grid[(x, y + size)] - grid[(x + size, y)] + grid[(x, y)]
                 if sum_fuel > max_fuel_2:
                     max_fuel_2 = sum_fuel
-                    max_fuel_loc_2 = (x, y)
+                    max_fuel_loc_2 = (x + 1, y + 1)
                     max_fuel_size_2 = size
 
+    answer1 = f"{max_fuel_loc[0]},{max_fuel_loc[1]}"
     answer2 = f"{max_fuel_loc_2[0]},{max_fuel_loc_2[1]},{max_fuel_size_2}"
 
     if submit_answer:
-        submit(max_fuel_loc, 1, day=11, year=2018)
+        submit(answer1, 1, day=11, year=2018)
         submit(answer2, 2, day=11, year=2018)
-    return max_fuel_loc, answer2
+    return answer1, answer2
