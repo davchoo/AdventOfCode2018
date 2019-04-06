2780442aa88e98d85fffcab588e5017821d2ef34
davchoo <davchoo3@gmail.com>
2019-04-06 01:26:23 -0700

    Clean up day 1
    Reduce total frequency loop to sum
    Separate part2 into find_repeating_freq and rename variables
    to better label them

--- day_1_2018.py
+++ day_1_2018.py
@@ -2,23 +2,24 @@ from aocd import get_data, submit
 import itertools
 
 
-def day1(submit_answer=False):
-    data = get_data(day=1, year=2018)
-    total_frequency = 0
-    frequencies = list(map(int, data.split("\n")))
-
-    for f in frequencies:
-        total_frequency += f
-
+def find_repeating_freq(delta_frequency):
     found_frequencies = set()
     frequency = 0
-    for f in itertools.cycle(frequencies):
-        frequency += f
+    for delta in itertools.cycle(delta_frequency):
+        frequency += delta
         if frequency not in found_frequencies:
             found_frequencies.add(frequency)
         else:
-            break
+            return frequency
+
+
+def day1(submit_answer=False):
+    data = get_data(day=1, year=2018)
+    delta_frequency = list(map(int, data.split("\n")))
+    total_frequency = sum(delta_frequency)
+    repeating_frequency = find_repeating_freq(delta_frequency)
+
     if submit_answer:
         submit(total_frequency, 1, day=1, year=2018)
-        submit(frequency, 2, day=1, year=2018)
-    return total_frequency, frequency
+        submit(repeating_frequency, 2, day=1, year=2018)
+    return total_frequency, repeating_frequency
